5f037baee1414ce0002c6341c8495add295d3c45
Lucas de Sena <lucas@seninha.org>
2023-04-29 07:26:05 -0300

    add defs.h

--- util.h
+++ util.h
@@ -4,20 +4,7 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define LEN(a)          (sizeof(a) / sizeof((a)[0]))
-#define FLAG(f, b)      (((f) & (b)) == (b))
-#ifndef RETURN_FAILURE
-#define RETURN_FAILURE  (-1)
-#endif
-#ifndef RETURN_SUCCESS
-#define RETURN_SUCCESS  0
-#endif
-#ifndef FALSE
-#define FALSE           0
-#endif
-#ifndef TRUE
-#define TRUE            1
-#endif
+#include "defs.h"
 
 pid_t efork(void);
 int max(int x, int y);
