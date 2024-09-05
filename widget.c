phillbush/xfiles

commit bee1a90782c6b2cf54fb703a2d3f5fff2cb78ebf

widget/dnd_event_handler scroll only when dragging over widget's window

widget.c
@@ -2664,13 +2664,10 @@ dnd_event_handler(XEvent *event, void *arg)
 		if (lasttime + SCROLL_TIME > event->xmotion.time)
 			break;
 		lasttime = event->xmotion.time;
-		if (y < SCROLL_STEP)
-			y = -SCROLL_STEP;
-		else if (widget->h - y < SCROLL_STEP)
-			y = +SCROLL_STEP;
-		else
-			break;
-		scroll(widget, y);
+		if (y >= 0 && y < SCROLL_STEP)
+			scroll(widget, -SCROLL_STEP);
+		else if (y >= widget->h - SCROLL_STEP && y < widget->h)
+			scroll(widget, +SCROLL_STEP);
 		break;
 	}
 	endevent(widget);

commit c6ecca5c435bbc65c788fea717127c103d6d8bab

bee1a90 broke _CONTROL_GOTO.  Gotta do the change I propose on #49 asap.

widget.c
@@ -2663,13 +2663,24 @@ close:
 				False
 			);
 			if (str == NULL)
-				return False;
+				break;
 			loadresources(widget, str);
 			FREE(str);
 			drawitems(widget);
 			widget->redraw = True;
+			break;
 		}
-		break;
+		if (ev->xproperty.window == widget->window &&
+		    ev->xproperty.atom == widget->atoms[_CONTROL_GOTO]) {
+			free(widget->gototext);
+			widget->gototext = gettextprop(
+				widget,
+				widget->window,
+				widget->atoms[_CONTROL_GOTO],
+				True
+			);
+		}
+		return False;
 	case SelectionRequest:
 		if (ev->xselectionrequest.owner != widget->window)
 			break;
@@ -2947,19 +2958,6 @@ mainmode(Widget *widget, int *selitems, int *nitems, char **text)
 	case CloseNotify:
 		return WIDGET_CLOSE;
 	case PropertyNotify:
-		if (ev.xproperty.state != PropertyNewValue)
-			continue;
-		if (ev.xproperty.window != widget->window)
-			continue;
-		if (ev.xproperty.atom != widget->atoms[_CONTROL_GOTO])
-			continue;
-		free(widget->gototext);
-		widget->gototext = gettextprop(
-			widget,
-			widget->window,
-			widget->atoms[_CONTROL_GOTO],
-			True
-		);
 		if (widget->gototext != NULL) {
 			*text = widget->gototext;
 			return WIDGET_GOTO;
@@ -3626,6 +3624,10 @@ widget_poll(Widget *widget, int *selitems, int *nitems, Scroll *scrl, char **tex
 	*text = NULL;
 	*nitems = 0;
 	retval = widget_wait(widget);
+	if (widget->gototext != NULL) {
+		*text = widget->gototext;
+		return WIDGET_GOTO;
+	}
 	if (retval == WIDGET_CLOSE || retval == WIDGET_ERROR)
 		return retval;
 	widget->start = True;